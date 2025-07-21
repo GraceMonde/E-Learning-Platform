bc949f07a70a9aa748a88e6e69f71bf107674360
Grace Kabwe <gracekbwe@gmail.com>
2025-07-21 20:26:12 +0200

    updating server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,23 @@
+// Load environment variables
 require('dotenv').config();
-const port = process.env.PORT;
-const dbHost = process.env.DB_HOST;
+
+// Load dependencies
+const express = require('express');
+const cors = require('cors');
+const db = require('./config/db');
+
+// Initialize app
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get('/', (req, res) => {
+    res.send('E-learning Platform API Running');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:5000`);
+});
